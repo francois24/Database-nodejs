29efe87dd1568ded9869cb79772fbecff457f389
Tom <ffrancois737@gmail.com>
2022-09-28 09:00:32 +0800

    node

---
+++ index.js
@@ -0,0 +1,34 @@
+
+var express = require ('express'),
+routes = require ('./routes'),
+path = require ('path'),
+fileUpload = require ('express-fileupload'),
+app = express(),
+mysql = require('mysql'),
+bodyParser = require('body-parser');
+
+var connection = mysql.createConnection({
+    host :'localhost',
+    user:'root',
+    password:'',
+    database:'student_records'
+});
+
+connection.connect();
+
+global.db = connection;
+
+app.set('port',process.env.PORT || 3000);
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'public')));
+app.use(fileUpload());
+
+app.get('/',routes.index);
+app.post('/',routes.index);
+app.get('/profile/:id',routes.profile);
+
+app.listen(3000);
+
